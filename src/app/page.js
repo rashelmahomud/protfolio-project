cf1348bdaae74bc270790eac69d6b657611f0ffc
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-01-05 15:19:16 +0600

    added skill site

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HomeAbout from "@/components/home/About";
 import Banner from "@/components/home/Banner";
+import Skill from "@/components/home/Skill";
 
 export default function Home() {
   return (
     <main>
       <Banner />
       <HomeAbout />
+      <Skill />
     </main>
   );
 }
