641819bf81d4dbe1aaeac1fe34304d2bed91fee1
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-01-19 06:54:29 +0600

    added some editing

--- src/components/shared/footer.jsx
+++ src/components/shared/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 bg-[#1A1E23] p-4 border-t-2 border-gray-700 text-gray-400 gap-5">
       <div>
-        <p>© 2023 rashel. All rights reserved.</p>
+        <p>© 2025 rashel. All rights reserved.</p>
       </div>
       <ul className="flex gap-2">
         <li>Privacy Policy</li>
--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -1,34 +1,70 @@
-import { faDiscord, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+"use client";
+import {
+  faDiscord,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { faBars, faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
 
-   
-
-    return (
-       <div className="bg-[#292F36] text-gray-300 font-semibold ">
-         <div className="container mx-auto flex justify-between py-12 px-2 border-b-2 border-gray-700">
-            <div>
-                <span>md.Rashel</span>
-            </div>
-            <div>
-                <p><Link href={'/'}><FontAwesomeIcon icon={faBars}  className="w-5 text-[#12F7D6] lg:hidden"/></Link></p>
-                <ul className="lg:flex gap-5 hidden">
-                    <li className="text-[#12F7D6]"><Link href={'/'}>Home</Link></li>
-            
-                    <li><Link href={'/blog'}>Blog</Link></li>
-                    <li className="relative"><Link href={'/search'}><FontAwesomeIcon icon={faMagnifyingGlass}className="w-5 absolute ml-40 mt-1 text-gray-500" /><input type="text" className="rounded-full outline-none"  placeholder=""/></Link></li>
-                    <li className="flex justify-center items-center gap-2"><FontAwesomeIcon icon={faInstagram}  className="w-5 text-[#12F7D6]"/><Link href={'/instagram'}>Instagram</Link></li>
-                    <li className="flex justify-center items-center gap-2"><FontAwesomeIcon icon={faDiscord} className="w-5 text-[#12F7D6]"/><Link href={'/discord'}>Discord</Link></li>
-                    <li className="flex justify-center items-center gap-2"><FontAwesomeIcon icon={faGithub} className="w-5 text-[#12F7D6]"/><Link href={'/github'}>Github</Link></li>
-                </ul>
-               
-            </div>
+  return (
+    <div className="bg-[#292F36] text-gray-300 font-semibold ">
+      <div className="container mx-auto flex lg:justify-between py-12 px-2 border-b-2 border-gray-700">
+        <div>
+          <span>md.Rashel</span>
         </div>
-       </div>
-    );
+        <div className="">
+          <div onClick={() => setOpen(!open)} className="w-5 text-[#12F7D6] lg:hidden mr-10">
+            {open ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />}
+          </div>
+
+          <ul className={`lg:flex gap-5 transition-all duration-500 ease-in ${open ? "top-18 opacity-100 " : "top-[-490px] lg:opacity-100 opacity-0"}`}>
+            <li className="text-[#12F7D6]">
+              <Link href={"/"}>Home</Link>
+            </li>
+
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
+            <li className="relative">
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  className="lg:w-5 absolute ml-40 mt-1 text-gray-500"
+                />
+                <input
+                  type="text"
+                  className="rounded-full outline-none"
+                  placeholder=""
+                />
+            </li>
+            <li className="flex justify-center items-center gap-2">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="w-5 text-[#12F7D6]"
+              />
+              <Link href={"/instagram"}>Instagram</Link>
+            </li>
+            <li className="flex justify-center items-center gap-2">
+              <FontAwesomeIcon
+                icon={faDiscord}
+                className="w-5 text-[#12F7D6]"
+              />
+              <Link href={"/discord"}>Discord</Link>
+            </li>
+            <li className="flex justify-center items-center gap-2">
+              <FontAwesomeIcon icon={faGithub} className="w-5 text-[#12F7D6]" />
+              <Link href={"/github"}>Github</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
