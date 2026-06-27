f9e00b93618974fe96f4c67235b3f0cd07db0627
Your Name <you@example.com>
2026-06-27 11:17:56 -0700

    update final

--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -1,100 +1,222 @@
 "use client";
-import {
-  faDiscord,
-  faGithub,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+
+import { useState } from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
   faMagnifyingGlass,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
-import { useState } from "react";
+
+import {
+  faDiscord,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
+  const closeMenu = () => setOpen(false);
+
   return (
-    <div className="bg-[#292F36] text-gray-300 font-semibold ">
-      <div className="container mx-auto flex lg:justify-between items-center py-12 px-2 border-b-2 border-gray-700">
-        <div className="lg:text-[25px] text-base">
-          <Link href={"/"}>
-            <span>md.Rashel</span>
-          </Link>
-        </div>
-        <div className="flex flex-row-reverse lg:ml-16">
-          <div
-            onClick={() => setOpen(!open)}
-            className="w-5 text-[#12F7D6] lg:hidden ml-10"
-          >
-            {open ? (
-              <FontAwesomeIcon icon={faXmark} />
-            ) : (
-              <FontAwesomeIcon icon={faBars} />
-            )}
-          </div>
+    <header className="sticky top-0 z-50 bg-[#292F36] shadow-lg border-b border-gray-700">
+      <nav className="container mx-auto flex items-center justify-between px-5 py-5 relative">
 
-          <ul
-            className={`lg:flex gap-5 transition-all duration-500 ease-in items-center lg:p-0 p-3 ${
-              open
-                ? "top-18 opacity-100"
-                : "top-[-490px] lg:opacity-100 opacity-0 lg:mt-0 -mt-40"
-            }`}
-          >
-            <li className="text-[#12F7D6] lg:text-[20px] text-base">
-              <Link href={"/"}>Home</Link>
-            </li>
+        {/* Logo */}
+        <Link
+          href="/"
+          className="text-xl lg:text-3xl font-bold text-white"
+          onClick={closeMenu}
+        >
+          <span className="text-[#12F7D6]">md.</span>Rashel
+        </Link>
 
-            <li className="lg:text-[20px] text-base">
-              <Link href={"/blogs"}>Blogs</Link>
-            </li>
-            <li className="relative lg:my-0 my-2">
-              <FontAwesomeIcon
-                icon={faMagnifyingGlass}
-                className="lg:w-5 absolute lg:ml-44 ml-28 lg:mt-2 mt-1 text-gray-500"
-              />
+        {/* Desktop Menu */}
+        <ul className="hidden lg:flex items-center gap-8 text-gray-300">
+
+          <li>
+            <Link
+              href="/"
+              className="hover:text-[#12F7D6] duration-300"
+            >
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/blogs"
+              className="hover:text-[#12F7D6] duration-300"
+            >
+              Blogs
+            </Link>
+          </li>
+
+          {/* Search */}
+          <li>
+            <div className="relative">
               <input
                 type="text"
-                className="rounded-full outline-none lg:w-[204px] w-[150px] lg:h-[32px] h-[22px] px-2 text-gray-900"
-                placeholder=""
+                placeholder="Search..."
+                className="w-56 rounded-full py-2 pl-4 pr-10 outline-none text-black"
               />
-            </li>
-            <li className="flex items-center gap-1 text-base">
+
               <FontAwesomeIcon
-                icon={faInstagram}
-                className="text-[20px] mr-1 text-[#12F7D6]"
+                icon={faMagnifyingGlass}
+                className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500"
               />
+            </div>
+          </li>
+
+          <li>
+            <Link
+              href="https://www.linkedin.com/in/rashelmahomud"
+              target="_blank"
+              className="flex items-center gap-2 hover:text-[#12F7D6]"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="text-[#12F7D6]"
+              />
+              LinkedIn
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="https://discord.com/users/"
+              target="_blank"
+              className="flex items-center gap-2 hover:text-[#12F7D6]"
+            >
+              <FontAwesomeIcon
+                icon={faDiscord}
+                className="text-[#12F7D6]"
+              />
+              Discord
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="https://github.com/rashelmahomud"
+              target="_blank"
+              className="flex items-center gap-2 hover:text-[#12F7D6]"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="text-[#12F7D6]"
+              />
+              Github
+            </Link>
+          </li>
+
+        </ul>
+
+        {/* Mobile Button */}
+
+        <button
+          className="lg:hidden text-[#12F7D6] text-2xl"
+          onClick={() => setOpen(!open)}
+        >
+          <FontAwesomeIcon icon={open ? faXmark : faBars} />
+        </button>
+
+        {/* Mobile Menu */}
+
+        <div
+          className={`lg:hidden absolute left-0 top-full w-full bg-[#292F36] border-t border-gray-700 overflow-hidden transition-all duration-300 ${
+            open ? "max-h-[500px]" : "max-h-0"
+          }`}
+        >
+          <ul className="flex flex-col gap-5 p-6 text-gray-300">
+
+            <li>
               <Link
-                href={"https://www.linkedin.com/in/rashelmahomud/"}
-                target="_blank"
+                href="/"
+                onClick={closeMenu}
+                className="hover:text-[#12F7D6]"
               >
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href="/blogs"
+                onClick={closeMenu}
+                className="hover:text-[#12F7D6]"
+              >
+                Blogs
+              </Link>
+            </li>
+
+            <li>
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="w-full rounded-full py-2 pl-4 pr-10 outline-none text-black"
+                />
+
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500"
+                />
+              </div>
+            </li>
+
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/rashelmahomud"
+                target="_blank"
+                onClick={closeMenu}
+                className="flex items-center gap-3 hover:text-[#12F7D6]"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className="text-[#12F7D6]"
+                />
                 LinkedIn
               </Link>
             </li>
-            <li className="flex items-center gap-1">
-              <FontAwesomeIcon
-                icon={faDiscord}
-                className="text-[20px] mr-1 text-[#12F7D6]"
-              />
+
+            <li>
               <Link
-                href={"https://discord.com/channels/@mdrashel"}
+                href="https://discord.com/users/"
                 target="_blank"
+                onClick={closeMenu}
+                className="flex items-center gap-3 hover:text-[#12F7D6]"
               >
+                <FontAwesomeIcon
+                  icon={faDiscord}
+                  className="text-[#12F7D6]"
+                />
                 Discord
               </Link>
             </li>
-            <li className="flex items-center gap-1">
-              <FontAwesomeIcon icon={faGithub} className="text-[20px] mr-1 text-[#12F7D6]" />
-              <Link href={"https://github.com/rashelmahomud"} target="_blank">
+
+            <li>
+              <Link
+                href="https://github.com/rashelmahomud"
+                target="_blank"
+                onClick={closeMenu}
+                className="flex items-center gap-3 hover:text-[#12F7D6]"
+              >
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="text-[#12F7D6]"
+                />
                 Github
               </Link>
             </li>
+
           </ul>
         </div>
-      </div>
-    </div>
+
+      </nav>
+    </header>
   );
 };
 
