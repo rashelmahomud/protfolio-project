29b0d36db4be7de79ca1f231ee3ffaa310638ec4
rashelmahomud <rashelmahmudraj1998@gmail.com>
2026-06-30 22:25:52 +0600

    added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,14 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-// export default eslintConfig;
-import { FlatCompat } from "@eslint/eslintrc"
-import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
-/** @type {import('eslint').Linter.Config[]} */
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-  ...compat.extends("next/typescript"), // Add this line if you are using TypeScript
-]
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-export default eslintConfig
+export default eslintConfig;
